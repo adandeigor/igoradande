9df5d54a0536ad94fe5a51eb2739eca91e56469e
adande igor <adandehustler@gmail.com>
2025-08-28 14:56:18 +0100

    correction

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offline-cache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
 };
 
 export default nextConfig
